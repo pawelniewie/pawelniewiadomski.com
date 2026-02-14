43ebffbd0fe396b8f007068df88ef260b1de32b3
Pawel Niewiadomski <11110000b@gmail.com>
2026-02-14 13:30:54 +0100

    Fix the error in the code (use Suspense before accessing useSearchParams)

--- src/lib/posthog.tsx
+++ src/lib/posthog.tsx
@@ -2,7 +2,7 @@
 
 import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
-import { useEffect } from 'react'
+import { useEffect, Suspense } from 'react'
 import { usePathname, useSearchParams } from 'next/navigation'
 
 // Initialize PostHog
@@ -38,7 +38,9 @@ function PostHogPageView() {
 export function PHProvider({ children }: { children: React.ReactNode }) {
   return (
     <PostHogProvider client={posthog}>
-      <PostHogPageView />
+      <Suspense fallback={null}>
+        <PostHogPageView />
+      </Suspense>
       {children}
     </PostHogProvider>
   )
